8c7041b5f5851aef397998418ce5a9cee6f227f4
abdullahaaf <abdullahaaf1@KeDATech.onmicrosoft.com>
2024-05-05 06:13:44 +0700

    update schema version from 45 to 37

--- web/app/Webtrees.php
+++ web/app/Webtrees.php
@@ -137,7 +137,7 @@ class Webtrees
     public const NAME = 'webtrees';
 
     // Required version of database tables/columns/indexes/etc.
-    public const SCHEMA_VERSION = 45;
+    public const SCHEMA_VERSION = 37;
 
     // e.g. "-dev", "-alpha", "-beta", etc.
     public const STABILITY = '';
